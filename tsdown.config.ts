31d58ed63d00393865e26a90604e4c35adcdd368
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-05-10 02:00:19 +0800

    feat: dts

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,14 +3,15 @@ import Vue from 'unplugin-vue/rolldown'
 
 export default defineConfig([
   {
-    entry: ['./dist/types/index.d.ts'],
-    dts: { dtsInput: true },
-    onSuccess: 'rm -fr dist/types',
-  },
-  {
     entry: ['./src/index.ts'],
-    target: 'node20.18',
     platform: 'neutral',
-    plugins: [Vue({ isProduction: true })],
+    plugins: [
+      Vue({
+        isProduction: true,
+      }),
+    ],
+    dts: {
+      vue: true,
+    },
   },
 ])
